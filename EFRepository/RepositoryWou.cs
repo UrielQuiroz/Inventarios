b440d1c7088ac0843b2c2e01a5947ed212ffb641
Uriel Quiroz <uriel.quiroz187@gmail.com>
2020-02-17 15:52:57 -0600

    Estructura del proyecto

---
+++ EFRepository/RepositoryWou.cs
@@ -0,0 +1,47 @@
+﻿using Repository;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFRepository
+{
+    public class RepositoryWou : Repository, IRepository, IUnitOfWork, IDisposable
+    {
+        public RepositoryWou(DbContext context, bool autoDetectChangesEnabled = false, bool proxCreationEnabled = false):
+            base(context, autoDetectChangesEnabled, proxCreationEnabled)
+        {
+
+        }
+        public int Save()
+        {
+            int Result = 0;
+            try
+            {
+                Result = Context.SaveChanges();
+            }
+            catch (DbEntityValidationException dbEx)
+            {
+                string strError = "";
+                foreach (var validationErrors in dbEx.EntityValidationErrors)
+                {
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                    {
+                        strError += string.Format("Property:{0} Error{1}",
+                            validationError.PropertyName,
+                            validationError.ErrorMessage);
+                    }
+                }
+            }
+            return Result;
+        }
+
+        protected override int TrySaveChanges()
+        {
+            return 0;
+        }
+    }
+}
